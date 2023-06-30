f1347fb60f5c3668bd594fa37f773e7572db0ca0
JavaBaz <Bazgir.civil@gmail.com>
2023-06-30 14:58:17 +0330

    Add Main.java file.

--- cw12-2/src/main/java/org/example/Main.java
+++ cw12-2/src/main/java/org/example/Main.java
@@ -1,16 +1,28 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
-        Thread Mother = new Thread(new Family());
-        Thread Father = new Thread(new Family());
-        Thread Son = new Thread(new Family());
+        Thread Mother = new Thread(new Family("Mother"));
+        Thread Father = new Thread(new Family("Father"));
+        Thread Son = new Thread(new Family("Son"));
 
         Mother.start();
         Father.start();
         Son.start();
+
+
+
+        Thread.sleep(5001);
+
+        if (!Mother.isAlive() && !Son.isAlive() && !Father.isAlive()) {
+            System.out.println("Breakfast is ready!!!");
+        }
+
+        else {
+            System.out.println("Breakfast is NOT ready!!!");
+        }
 
 
     }
