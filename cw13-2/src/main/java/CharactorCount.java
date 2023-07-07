3821df077e53a3d8eecce708f35718b97a2626aa
navid git <navid.nikfar@gmail.com>
2023-07-07 10:12:36 +0330

    navid joon push kardeee

---
+++ cw13-2/src/main/java/CharactorCount.java
@@ -0,0 +1,22 @@
+public class CharactorCount implements Runnable {
+    private String sentences;
+    private char c;
+
+    int counter=0;
+
+    public CharactorCount(String sentences , char c){
+        this.sentences=sentences;
+        this.c=c;
+    }
+    @Override
+    public void run(){
+        for (int i = 0; i < sentences.length(); i++) {
+            if (sentences.charAt(i)==c){
+                counter++;
+            }
+        }
+        System.out.println("charactor count is : "+ counter);
+    }
+
+    // initialize counter in method or out of method for part 3
+}
---
+++ cw13-2/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        String sentences = "salaaam bacheha";
+        char c = 'a';
+        //Thread thread = new Thread(new CharactorCount(sentences,c));
+        //  thread.start();
+        int mainCounter=0;
+        ExecutorService ex = Executors.newSingleThreadExecutor();
+        try {
+            ex.submit(new CharactorCount(sentences, c));
+            ex.shutdown();
+            ex.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            System.err.println(e);
+        } finally {
+            if (!ex.isTerminated()) {
+                System.err.println("cancel tasks");
+            }
+            ex.shutdownNow();
+            System.out.println(" finished");
+        }
+
+    }
+
+}
