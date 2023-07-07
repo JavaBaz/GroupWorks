fe8bc0a61b851b3d90b71590452be24f0c85fc8a
JavaBaz <Bazgir.civil@gmail.com>
2023-07-07 14:50:12 +0330

    cw13-8.

---
+++ cw13-7/src/main/java/com/hw11/Main.java
@@ -0,0 +1,15 @@
+package com.hw11;
+
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+
+        ClassForInterface testClass = new ClassForInterface();
+
+
+        TestInterface.staticMehod();
+
+    }
+}
---
+++ cw13-8/src/main/java/com/hw11/Main.java
@@ -0,0 +1,20 @@
+package com.hw11;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+    Person Naghi = () -> new Date();
+
+
+
+
+
+
+
+    Person Taghi = () -> new Date();
+
+
+    }
+}
