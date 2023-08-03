baf34941c380e1a93a01124399cccfca7de326d7
saber <sbaerm.com@gmail.com>
2023-08-03 16:47:56 +0330

    saber

--- cw13-2/src/main/java/CharactorCount.java
+++ cw13-2/src/main/java/CharactorCount.java
@@ -2,20 +2,21 @@ public class CharactorCount implements Runnable {
     private String sentences;
     private char c;
 
-    int counter=0;
+    int counter = 0;
 
-    public CharactorCount(String sentences , char c){
-        this.sentences=sentences;
-        this.c=c;
+    public CharactorCount(String sentences, char c) {
+        this.sentences = sentences;
+        this.c = c;
     }
+
     @Override
-    public void run(){
+    public void run() {
         for (int i = 0; i < sentences.length(); i++) {
-            if (sentences.charAt(i)==c){
+            if (sentences.charAt(i) == c) {
                 counter++;
             }
         }
-        System.out.println("charactor count is : "+ counter);
+        System.out.println("charactor count is : " + counter);
     }
 
     // initialize counter in method or out of method for part 3
