ab0f72cc78bc723dea43e377627436cdd1daf809
JavaBaz <Bazgir.civil@gmail.com>
2023-08-04 15:17:06 +0330

    DONE : 16-5 part one.

---
+++ cw16-5/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("postgres");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+
+    }
+}
---
+++ cw16-5/src/main/java/org/example/model/Film.java
@@ -0,0 +1,104 @@
+package org.example.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Film {
+
+    @Id
+    private Long id;
+    public String title;
+    public String description;
+    public int length;
+    public String language;
+    public int rating;
+    public String director;
+
+
+
+    public Film(String title, String description, int length, String language, int rating, String director) {
+        this.title = title;
+        this.description = description;
+        this.length = length;
+        this.language = language;
+        this.rating = rating;
+        this.director = director;
+    }
+
+    public Film() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", length=" + length +
+                ", language='" + language + '\'' +
+                ", rating=" + rating +
+                ", director='" + director + '\'' +
+                '}';
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+
+}
