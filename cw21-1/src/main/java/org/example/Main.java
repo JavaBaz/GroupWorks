ab626f5f476800f2e2ea64f4d82d2d1e9cac3614
JavaBaz <Bazgir.civil@gmail.com>
2023-09-22 09:38:15 +0330

    Finish cw21-1.

--- cw21-1/src/main/java/org/example/Main.java
+++ cw21-1/src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
 
+
+
+
+
 import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 
 import java.util.Scanner;
 
+
+
 public class Main {
 
-    private static final Logger logger = Logger.getLogger(Main.class);
+
+    final static Logger logger = Logger.getLogger(Main.class);
 
 
 
     public static void main(String[] args) {
+        DOMConfigurator.configure("cw21-1/src/main/resources/log4j.properties");
+
         System.out.println(" Welcome to Java Calculator v0.1 \n");
         Scanner scanner = new Scanner(System.in);
 
@@ -27,7 +37,9 @@ public class Main {
 
         switch (operation) {
             case "*":
-                System.out.println(eo + "* \n Your Result: "+ ( firstNumber * secondNumber ));
+                System.out.println(eo + "*");
+                mul(firstNumber, secondNumber);
+
                 break;
             case "/":
                 System.out.println(eo + "/ \n Your Result: "+ ( firstNumber / secondNumber ));
@@ -51,10 +63,10 @@ public class Main {
     }
 
 
-    public void mul(int num1, int num2){
+    public static void mul(int num1, int num2){
         logger.info("this is an info log");
         int result = num1 * num2;
-        System.out.println(result);
+        System.out.println("Your Result: "+result);
         logger.debug("this is an info log");
     }
 
